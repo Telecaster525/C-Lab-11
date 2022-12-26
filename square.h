0f45897f6e81401025c64e23b30e883531b451a0
Said Gadzhiev <94011547+Telecaster525@users.noreply.github.com>
2022-12-27 00:35:15 +0300

    Create square.h

---
+++ square.h
@@ -0,0 +1,12 @@
+#ifndef INC_11LAB_SQUARE_H
+#define INC_11LAB_SQUARE_H
+
+struct Square{
+double x1, y1;
+double x2, y2;
+};
+
+double Perimeter(struct Square);
+double Square(struct Square);
+
+#endif //INC_11LAB_SQUARE_H
